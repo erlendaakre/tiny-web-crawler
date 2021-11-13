501609bd129beb78f56d07aa860683bca25c8b58
Erlend Aakre <erlend@aakre.io>
2021-11-13 13:01:38 +0000

    single url crawl, no parallelism

--- src/main/scala/io/aakre/tiny_web_crawler/Crawler.scala
+++ src/main/scala/io/aakre/tiny_web_crawler/Crawler.scala
@@ -1,10 +1,52 @@
-package main.scala.io.aakre.tiny_web_crawler
-import zio.{ExitCode, URIO, ZIO}
+package io.aakre.tiny_web_crawler
 
-object Crawler extends zio.App {
-  override def run(args: List[String]): URIO[zio.ZEnv, ExitCode] = {
-    ZIO.succeed {
-      print("TODO: stuff!")
-    }.exitCode
+import zio._
+import zio.Console._
+
+import java.net.URL
+
+object Crawler extends zio.ZIOAppDefault {
+  private val SampleUrl = "https://monzo.com/"
+  private val crawlerParallelism = 4
+
+  def run =
+    for {
+      _    <- printLine("Tiny web crawler")
+      _    <- print(s"URL [$SampleUrl]:")
+      url  <- getUrl
+      _    <- printLine(s"Crawling $url...")
+      res    <- crawl(url)
+      _   <- printLine(res)
+    } yield ()
+
+  private def getUrl  = {
+    readLine.map(s => if(s.isBlank) SampleUrl else s).map(s => if(s.endsWith("/")) s else s"$s/")
+      .mapAttempt(s => new URL(s)).catchAll {
+      _ => printLine(s"Error, invalid URL, using fallback: $SampleUrl") *> ZIO.succeed(new URL(SampleUrl))
+    }
   }
+
+  private def crawl(startUrl: URL) = {
+    for {
+      body  <- readPage(startUrl)
+      links <- extractLinks(body, startUrl)
+    } yield (startUrl, links.flatten)
+  }
+
+  private def extractLinks(html: String, baseUrl: URL) =
+    ZIO.attempt(UrlPattern.findAllIn(html).toList.map(filterAndCreateURLs(_, baseUrl)))
+
+  private def filterAndCreateURLs(s: String, baseUrl: URL): Option[URL] = {
+    val trimmed = if(s.startsWith("<a href=")) s.substring(9).dropRight(1) else s.dropRight(1)
+
+    if(trimmed.isBlank || trimmed == "/" || trimmed == "#") None
+    else {
+      if(trimmed.startsWith(baseUrl)) Some(new URL(trimmed))
+      else Some(new URL(baseUrl + (if(trimmed.startsWith("/")) trimmed.drop(1) else trimmed)))
+    }
+  }
+
+  private def readPage(url: URL) = ZIO.attempt(requests.get(url).text)
+
+  type Result = Map[URL, List[URL]]
 }
