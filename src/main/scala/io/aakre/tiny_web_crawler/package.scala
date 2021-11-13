0cfbddee29fbf8fc0327525c3060ec6ae0354d25
Erlend Aakre <erlend@aakre.io>
2021-11-13 13:00:43 +0000

    regex simplified

--- src/main/scala/io/aakre/tiny_web_crawler/package.scala
+++ src/main/scala/io/aakre/tiny_web_crawler/package.scala
@@ -6,6 +6,6 @@ import scala.util.matching.Regex
 
 package object tiny_web_crawler {
   implicit def JavaURLtoString(url: URL): String = url.toString
-  
-  val UrlPattern: Regex = """<a href=[\"\']([^\"\']+)[\"\']""".r
+
+  val UrlPattern: Regex = """<a href=["']([^"']+)["']""".r
 }
