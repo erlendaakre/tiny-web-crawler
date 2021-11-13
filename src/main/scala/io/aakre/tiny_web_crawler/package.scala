b3158831eb7f0b51b1946c5422044b679e0ad841
Erlend Aakre <erlend@aakre.io>
2021-11-13 12:59:55 +0000

    regex for extracting a href urls

--- src/main/scala/io/aakre/tiny_web_crawler/package.scala
+++ src/main/scala/io/aakre/tiny_web_crawler/package.scala
@@ -2,7 +2,10 @@ package io.aakre
 
 import java.net.URL
 import scala.language.implicitConversions
+import scala.util.matching.Regex
 
 package object tiny_web_crawler {
   implicit def JavaURLtoString(url: URL): String = url.toString
+  
+  val UrlPattern: Regex = """<a href=[\"\']([^\"\']+)[\"\']""".r
 }
